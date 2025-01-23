2a53d3a102d63ebbc406354dec2202b0374a6ecc
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-01-23 22:41:37 +0200

    [DisplayServer] Decouple `show_window(MAIN_WINDOW_ID)` from `DisplayServer` constructor, update project manager size/position after DS init.

--- platform/windows/display_server_windows.cpp
+++ platform/windows/display_server_windows.cpp
@@ -6898,7 +6898,6 @@ DisplayServerWindows::DisplayServerWindows(const String &p_rendering_driver, Win
 	}
 
 	windows[MAIN_WINDOW_ID].initialized = true;
-	show_window(MAIN_WINDOW_ID);
 
 #if defined(RD_ENABLED)
 	if (rendering_context) {
